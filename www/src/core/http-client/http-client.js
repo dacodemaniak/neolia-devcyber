2fa96d156186b9c06c76f9ebb3bfd86acf901fc6
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2024-06-05 08:58:58 +0200

    HttpClient service Update login to httpcall

---
+++ www/src/core/http-client/http-client.js
@@ -0,0 +1,91 @@
+import { from, map, Observable, of, throwError } from "rxjs"
+
+export class HttpClient {
+    #_method  = 'GET'
+    #_uri = ''
+    #_contentType = 'application/json'
+    #_body = ''
+    #_fetchOptions = {}
+
+    /**
+     * 
+     * @param {string} uri 
+     * @returns Observable<any>
+     */
+    get(uri) {
+        this.#_uri = uri
+
+        this.#_fetchOptions = {
+            method: 'get',
+            mode: 'cors',
+            headers: {
+                "Content-Type": this.contentType
+            }
+        }
+
+        return this.#send()
+
+    }
+
+    /**
+     * 
+     * @param {string} uri 
+     * @param {any} body 
+     * @returns Observable<any>
+     */
+    post(uri, body) {
+        this.#_method = 'post'
+        this.#_uri = uri
+        this.#_body = JSON.stringify(body)
+
+        this.#_fetchOptions = {
+            method: 'post',
+            mode: 'cors',
+            headers: {
+                "Content-Type": this.contentType
+            },
+            body: this.body
+        }
+        return this.#send()
+        
+    }
+
+    /**
+     * 
+     * @param {string} uri 
+     * @param {any} body 
+     * @returns Observable<any>
+     */
+    put(uri, body) {
+        this.method = 'put'
+        this.uri = uri
+        
+        this.body = JSON.stringify(body)
+
+        this.fetchOptions = {
+            method: 'put',
+            mode: 'cors',
+            headers: {
+                "Content-Type": this.contentType
+            },
+            body: this.body
+        }
+        return this.#send()        
+    }
+
+   #send() {
+        const apiCall = fetch(
+            this.#_uri,
+            this.#_fetchOptions
+        )
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw new Error(`Something went wrong calling ${this.method.toUpperCase()} ${this.uri} (${JSON.stringify(response)})`)
+        })
+        .then((responseJson) => responseJson)
+
+        return from(apiCall)
+    }
+}
--- www/src/login-form/login-service.js
+++ www/src/login-form/login-service.js
@@ -1,7 +1,25 @@
 import { of } from 'rxjs'
-
+import { HttpClient } from '../core/http-client/http-client'
 export class LoginService {
+    #protocol = 'http'
+    #apiHost = 'localhost'
+    #apiPort = 8003
+    #apiEndpoint = 'signin'
+
+    #uri = ''
+
+    #httpClient = null
+
+    constructor() {
+        this.#httpClient = new HttpClient()
+        this.#uri = `${this.#protocol}://${this.#apiHost}:${this.#apiPort}/`
+    }
+
     signin(value) {
-        return of(`Signin complete with ${JSON.stringify(value)}`)
+        const uri = `${this.#uri}${this.#apiEndpoint}`
+        return this.#httpClient.post(
+            uri,
+            value
+        )
     }
 }
