3fe483ec7d5ad866d9551107a7aa63ea530c34af
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2024-06-06 13:26:26 +0200

    Fix SQL script

--- docker/init.db/init.sql
+++ docker/init.db/init.sql
@@ -7,36 +7,42 @@ CREATE DATABASE IF NOT EXISTS dev_cyber_repository;
 
 USE dev_cyber_repository;
 
+DROP TABLE IF EXISTS dev_cyber_repository.user;
 CREATE TABLE dev_cyber_repository.user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     login VARCHAR(45) NOT NULL,
     password VARCHAR(64) NOT NULL
 );
 
+DROP TABLE IF EXISTS dev_cyber_repository.account;
 CREATE TABLE dev_cyber_repository.account (
     id INT AUTO_INCREMENT PRIMARY KEY,
     lastname VARCHAR(45) NOT NULL,
     firstname VARCHAR(30),
     gender SMALLINT NOT NULL default 0,
     user_id INT NOT NULL,
-    UNIQUE user_id,
+    UNIQUE (user_id),
     FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
+DROP TABLE IF EXISTS dev_cyber_repository.role;
 CREATE TABLE dev_cyber_repository.role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     role VARCHAR(20) NOT NULL,
-    UNIQUE role
+    UNIQUE (role)
 );
 
+DROP TABLE IF EXISTS dev_cyber_repository.user_has_role;
 CREATE TABLE dev_cyber_repository.user_has_role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     role_id INT NOT NULL,
-    UNIQUE (user_id, role_id)
+    UNIQUE (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
-INSERT INTO dev_cyber_repository.user (name) VALUES 
+INSERT INTO dev_cyber_repository.user (login, password) VALUES 
     ('admin', 'super_admin'),
     ('a.dupont', 'DuP0nT!');
 
@@ -48,7 +54,7 @@ INSERT INTO dev_cyber_repository.role (role) VALUES
     ('Administrateur'),
     ('Utilisateur');
 
-INSERT INTO dev_cyber_repository.user_has_role (user_id, role) VALUES 
+INSERT INTO dev_cyber_repository.user_has_role (user_id, role_id) VALUES 
     (1, 1),
     (1, 2),
     (2, 2);
